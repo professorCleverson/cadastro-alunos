35246be61849c990634d63e7dcdbd4e2625b0c86
Cleverson <cleverson.silva@msn.com>
2026-05-20 10:08:16 -0400

    .

--- src/main/java/br/com/escola/controller/AlunoController.java
+++ src/main/java/br/com/escola/controller/AlunoController.java
@@ -18,49 +18,67 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.escola.model.Aluno;
 import br.com.escola.repository.AlunoRepository;
 
-@Controller // Diz ao Spring: “Essa classe controla requisições web.” 
-			//O Spring registra automaticamente essa classe para responder URLs.
+@Controller // Diz ao Spring: “Essa classe controla requisições web.”
+			// O Spring registra automaticamente essa classe para responder URLs.
 public class AlunoController {
 
 	@Autowired // Faz injeção automática de dependência.
-               // O Spring cria o objeto AlunoRepository e coloca aqui automaticamente
+				// O Spring cria o objeto AlunoRepository e coloca aqui automaticamente
 	private AlunoRepository repository;
 
 	@GetMapping("/") // Mapeia requisições GET para a URL: http://localhost:8080/
 	public String inicio() {
-		return "redirect:/alunos"; //Redireciona o navegador para: /alunos
+		return "redirect:/alunos"; // Redireciona o navegador para: /alunos
 	}
 
 	@GetMapping("/alunos") // Mapeia requisições GET para quando alguém acessa: /alunos
 	public String listar(Model model) {
 
-		List<Aluno> lista = repository.findAll(); //Usa o JpaRepository para buscar todos os registros do banco.
+		List<Aluno> lista = repository.findAll(); // Usa o JpaRepository para buscar todos os registros do banco.
 
-		model.addAttribute("alunos", lista); //Coloca a lista dentro do objeto Model e Envia dados para a View.
+		model.addAttribute("alunos", lista); // Coloca a lista dentro do objeto Model e Envia dados para a View.
 
-		return "lista"; //Retorna a página templates/lista.html
+		return "lista"; // Retorna a página templates/lista.html
 	}
 
-	@GetMapping("/novo") //Abre o formulário para cadastrar aluno.
+	@GetMapping("/novo") // Abre o formulário para cadastrar aluno.
 	public String novo(Model model) {
 
-		model.addAttribute("aluno", new Aluno()); //Envia um objeto vazio para o formulário HTML.
+		model.addAttribute("aluno", new Aluno()); // Envia um objeto vazio para o formulário HTML.
 
-		return "formulario"; //Retorna página formulário templates/formulario.html
+		return "formulario"; // Retorna página formulário templates/formulario.html
 	}
 
 	@PostMapping("/salvar")
-	public String salvar(@ModelAttribute Aluno aluno) { //Recebe os dados enviados pelo formulário.
+	public String salvar(@ModelAttribute Aluno aluno) { // Recebe os dados enviados pelo formulário.
 
-		repository.save(aluno); //Salva no banco. O JPA salva o aluno no banco.
+		repository.save(aluno); // Salva no banco. O JPA salva o aluno no banco.
 
-		return "redirect:/alunos"; // Após salvar: 
-                                  // volta para a listagem;
-		                          // evita duplicação ao atualizar a página.
+		return "redirect:/alunos"; // Após salvar:
+									// volta para a listagem;
+									// evita duplicação ao atualizar a página.
 	}
+
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable Long id) {
+		repository.deleteById(id);
+		return "redirect:/aluno";
+	}
+
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable Long id, Model model) {
+
+		Aluno aluno = repository.findById(id).orElse(null);
+
+		model.addAttribute("aluno", aluno);
+
+		return "formulario";
+	}
+
 }
--- src/main/java/br/com/escola/model/Aluno.java
+++ src/main/java/br/com/escola/model/Aluno.java
@@ -4,9 +4,11 @@
  */
 package br.com.escola.model;
 
+import java.sql.Date;
+
 import jakarta.persistence.*;
 
-@Entity // Essa anotação diz ao JPA: “Essa classe deve virar uma tabela no banco de dados.”
+@Entity // Anotação diz ao JPA: “Essa classe deve virar uma tabela no banco de dados.”
 public class Aluno {
 
 	@Id //Indica que esse atributo é a chave primária da tabela.
@@ -15,6 +17,8 @@ public class Aluno {
 	private String nome;
 	private String email;
 	private String curso;
+	private String telefone;
+	private Date data_nascimento;
 
 	public Aluno() { //O JPA exige um construtor vazio. O Hibernate usa ele para criar objetos automaticamente quando lê dados do banco.
 
@@ -57,5 +61,25 @@ public class Aluno {
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
+	
+	
+	
 
 }
